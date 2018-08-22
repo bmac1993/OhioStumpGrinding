d9064172aefa7efd4c87e502210785917fd0c9c5
bmcalees <bmcalees@gmail.com>
2018-08-21 21:14:02 -0400

    Updating speed of carousel

--- Index.js
+++ Index.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function () {
-	$('.carousel').carousel()
+	$('.carousel').carousel({
+		interval:3000
+	});
 
 	$(document).on('click', '[data-toggle="lightbox"]', function (event) {
 		event.preventDefault();
